43b9333b86b083a81a9144f380157ba25d1e3c2f
ZuzannaNogala <zuzanna02nogala@gmail.com>
2024-05-11 11:16:23 +0200

    update function visual_boxplot

--- R/VisualizationBoxplot.R
+++ R/VisualizationBoxplot.R
@@ -6,21 +6,27 @@
 #' @param data data table or data frame, cointains names of variables
 #' @param str_name_Y character, name of dependent variable Y which takes the values 0 and 1
 #' @param str_name_X character, name of independent variable/predictor X
+#' @param plot_title character, optional, title of box plot
 #' 
 #' @import ggplot2
+#' @import patchwork
+#' @details
+#' package patchwork is available to simplify displaying graphs
 #' @examples
 #' citrus
 #' str(citrus)
-#' visual_boxplot(data = citrus, str_name_Y = "nameBin", str_name_X ="diameter")
+#' p1 <- visual_boxplot(data = citrus, str_name_Y = "nameBin", str_name_X ="diameter")
+#' p2 <- visual_boxplot(data = citrus, str_name_Y = "nameBin", str_name_X ="green")
+#' p1 + p2
 #' @export
-visual_boxplot <- function(data, str_name_Y, str_name_X){
+visual_boxplot <- function(data, str_name_Y, str_name_X, plot_title = paste("Box plot of", str_name_X, "versus", str_name_Y)){
   ggplot(data, aes(x = as.character(get(str_name_Y)), 
                    y = get(str_name_X), 
                    fill = as.character(get(str_name_Y)))) +
     geom_boxplot() +
     xlab(str_name_Y) +
     ylab(str_name_X) +
-    ggtitle(paste("Box plot of", str_name_X, "versus", str_name_Y)) +
+    ggtitle(plot_title) +
     theme_minimal() +
     scale_fill_brewer(palette="Set2") +
     guides(fill=guide_legend(title=str_name_Y))
