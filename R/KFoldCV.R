35aea23ebde23a7c3895e1a85751b6d2604dbf81
oktawiahankus <163405012+oktawiahankus@users.noreply.github.com>
2024-05-12 19:37:53 +0200

    Update KFoldCV.R

--- R/KFoldCV.R
+++ R/KFoldCV.R
@@ -5,16 +5,17 @@
     stop(paste0("Number of rows: ", rowNum, " is samller than ", K))
   }else if(rowNum %% K != 0){
     warning(paste0("Number of rows: ", rowNum, " is not divided by: ", K, 
-                   ". Your data won't be evenly divided."))
+                   ". Your data isn't evenly divided."))
   }
   
   indexes <- 1:rowNum
   shuffledRows <- sample(indexes, rowNum)
   
   partitionIdx <- split(indexes, cut(indexes, K))
+  names(partitionIdx) <- NULL
   
   splittedSet <- lapply(partitionIdx, function(idx){
-    shuffledRows[idx]
+    data[shuffledRows[idx]]
   })
   
   splittedSet
