fb773090fd479a3a826a0828d7c580c239fac884
oktawiahankus <163405012+oktawiahankus@users.noreply.github.com>
2024-05-12 21:14:44 +0200

    created and documented kFoldCV
    function for K-Fold Cross Validation

--- R/KFoldCV.R
+++ R/KFoldCV.R
@@ -1,3 +1,14 @@
+#' Splitting function
+#' 
+#' \code{.splitSet} gives partition of shuffled indexes of data table, to use 
+#' when subsets of data are needed
+#' 
+#' @param data data.frame or data.table we want to split
+#' @param K integer, number if partitions
+#' 
+#' @return list of groups of indexes for creating subsets of given data
+#' 
+#' @keywords internal
 .splitSet <- function(data, K){
   rowNum <- nrow(data)
   
@@ -15,10 +26,60 @@
   names(partitionIdx) <- NULL
   
   splittedSet <- lapply(partitionIdx, function(idx){
-    data[shuffledRows[idx]]
+    shuffledRows[idx]
   })
   
   splittedSet
 }
 
-
+#' K-Fold Cross Validation
+#' 
+#' Implementation of K-Fold Cross Validation technique for logistic regression. 
+#' In this technique, the parameter K refers to the number of different subsets 
+#' that the given data set is to be split into. Further, K-1 subsets are used 
+#' to train the model and the left out subsets are used as a validation set. For
+#' each subset we count accuracy, then we get accuracy score by applying mean 
+#' to all the accuracies received for all folds.
+#' 
+#' @param data data.table, set of data we want apply logistic regresion to
+#' @param K integer, number of folds
+#' @param threshold numeric from 0 to 1, threshold of success' probability - 
+#' if predicted probability of dependent variable is higher than treshold, the 
+#' event is counted as a success
+#' @param strNameY character, name of dependent variable Y which takes the values 0 and 1
+#' @param pred character vector, vector of predictors' names, 
+#' must be columns of \code{data}
+#' 
+#' @return numeric, accuracy score
+#' 
+#' @import data.table
+#' @importFrom stats glm
+#' 
+#' @examples
+#' kFoldCV(citrus, 4, 0.5, "nameBin", c("red", "blue"))
+#' 
+#' @export
+kFoldCV <- function(data, K, threshold, strNameY, pred){
+  rowNum <- nrow(data)
+  formula <- .getModelFormula(strNameY, pred)
+  modelData <- data[, .SD, .SDcols = c(strNameY, pred)]
+  
+  splittedIdx <- .splitSet(modelData, K)
+  
+  calc <- lapply(splittedIdx, function(idx){
+    test <- data[idx]
+    training <- data[-idx]
+    sizeTest <- length(idx)
+    
+    trueValues <- test[[strNameY]]
+    
+    model <- stats::glm(formula, training, family = "binomial")
+    prediction <- predict.glm(model, test, type = "response")
+    prediction <- ifelse(prediction > threshold, 1, 0)
+    
+    acc <- sum(trueValues == prediction) / sizeTest
+    acc
+  })
+  
+  mean(unlist(calc))
+}
