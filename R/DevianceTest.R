5ecb868c3aa50d9d3fb3c2a19605e4ca6ed013ef
ZuzannaNogala <zuzanna02nogala@gmail.com>
2024-05-21 21:04:47 +0200

    repair deviance test

--- R/DevianceTest.R
+++ R/DevianceTest.R
@@ -18,7 +18,7 @@
 #' @examples
 #' mdl_full <- glm(nameBin ~ diameter + green + blue + weight, 
 #'                  data = citrus, family = binomial("logit"))
-#' mdl_unfull <- glm(nameBin ~ diameter + weight + green, 
+#' mdl_unfull <- glm(nameBin ~ weight + green, 
 #'                  data = citrus, family = binomial("logit"))
 #'
 #' deviance_test(model_H0 = mdl_unfull, model_H1 = mdl_full, alpha = 0.1)
@@ -27,10 +27,11 @@ deviance_test <- function(model_H0, model_H1, alpha = 0.05){
   deviance_stat <- model_H0$deviance - model_H1$deviance
   df <- length(model_H1$coefficients) - length(model_H0$coefficients)
   p_val <- 1 - pchisq(deviance_stat, df)
-  num_testing_params <- setdiff(1:length(model_H1$coefficients), 
-                                which(names(model_H0$coefficients) %in% names(model_H1$coefficients)))
   
-  list("Tested_predictors" = c(names(model_H1$coefficients)[num_testing_params]), 
+  params_names <- union(names(model_H0$coefficients), names(model_H1$coefficients))
+  testing_params_names <- setdiff(param_names, names(model_H0$coefficients))
+  
+  list("Tested_predictors" = testing_params_names, 
        "Deviance_statistic" = deviance_stat, 
        "df" = df,
        "Critical_value" = qchisq(1 - alpha, df),
