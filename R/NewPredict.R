5cc693b3936f878b57a26515a983c1e0b3a12888
oktawiahankus <163405012+oktawiahankus@users.noreply.github.com>
2024-06-03 19:37:48 +0200

    added function for predicting new values

---
+++ R/NewPredict.R
@@ -0,0 +1,26 @@
+#' Prediction of new values
+#' 
+#' @param object a fitted object of class inheriting from "logisMod'
+#' @param newdata da data frame in which to look for variables with which to predict
+#' @param ... other parameters
+#' 
+#' @importFrom stats predict.glm
+#' 
+#' @examples
+#' library(data.table)
+#' model <- logisMod(nameBin ~ red, citrus)
+#' predData <- citrus[["red"]]
+#' newData <- list()
+#' newData[["red"]] <- seq(min(predData), max(predData), 0.1)
+#' newData <- as.data.table(newData)
+#' 
+#' predict(model, newData)
+#' 
+#' @details
+#' This function is fixed form of \code{stats::predict.glm}. For more details or 
+#' different version see \link[stats]{predict.glm}.
+#' 
+#' @export
+predict.logisMod <- function(object, newdata, ...){
+  stats::predict.glm(object, newdata = newData, type = "response")
+}
